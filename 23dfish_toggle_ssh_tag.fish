e15e1d2dcc20c16f16b521ce63c86cecc5b732dc
Anders Svensson <a@23d.one>
2020-08-16 10:27:00 -0700

    2020-08-16

--- 23dfish_toggle_ssh_tag.fish
+++ 23dfish_toggle_ssh_tag.fish
@@ -1,4 +1,4 @@
-function 23dfish_toggle_ssh_tag -d "Toggle  tag display."
+function 23dfish_toggle_ssh_tag -d "Toggle SSH tag display."
   if test "$THEME_23DFISH_HIDE_SSH_TAG" = 'yes'
     set -e THEME_23DFISH_HIDE_SSH_TAG
   else
