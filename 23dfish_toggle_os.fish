b496d7aa65aaa1258f3c9cb40d551a221bb2dea3
Anders Svensson <a@23d.one>
2020-08-16 21:31:32 -0700

    Update 23dfish_toggle_os.fish

--- 23dfish_toggle_os.fish
+++ 23dfish_toggle_os.fish
@@ -1,5 +1,5 @@
 function 23dfish_toggle_os -d "Toggle OS icon."
-  if test "$THEME_23DFISH_SHOW_OS" != 'yes'
+  if test "$THEME_23DFISH_SHOW_OS" = 'yes'
     set -e THEME_23DFISH_SHOW_OS
   else
     set -g THEME_23DFISH_SHOW_OS yes
