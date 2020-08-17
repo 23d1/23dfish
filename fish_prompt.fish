42350104612cbac61109b0432a4fea1fbfae47aa
Anders Svensson <a@23d.one>
2020-08-16 21:56:45 -0700

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -93,9 +93,9 @@ function show_ssh_status -d "Function to show the ssh tag"
   if test "$THEME_23DFISH_HIDE_SSH_TAG" != 'yes'
     if [ -n "$SSH_CLIENT" ]
       if [ (id -u) = "0" ]
-        _prompt_segment red white "  ssh " ' '
+        _prompt_segment red white "  " ' '
       else
-        _prompt_segment blue white "  ssh " ' '
+        _prompt_segment blue white "  " ' '
       end
     end
   end
