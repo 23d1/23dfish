4af532af0b04aa40559aa7ec3b0033dfefbfa14e
Anders Svensson <a@23d.one>
2020-08-17 23:45:25 -0700

    Update fish_title.fish

--- fish_title.fish
+++ fish_title.fish
@@ -1,3 +1,7 @@
 function fish_title
-# Customize the title bar of the terminal window.
+    # Customize the title bar of the terminal window.
+    echo "$USER@"
+    hostname
+    echo ' ' (status current-command) ' '
+    pwd
 end
